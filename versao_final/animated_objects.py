22b545677d8ad8348306538116428ab808d1a6eb
Lívia <livia@email.com>
2022-07-17 18:43:51 -0300

    primeiro commit

---
+++ versao_final/animated_objects.py
@@ -0,0 +1,62 @@
+import pygame
+from abc import ABC, abstractmethod
+from os import walk
+
+class AnimatedObject(pygame.sprite.Sprite, ABC):
+    @abstractmethod
+    def __init__(self, size, position, path):
+        super().__init__()
+        self.__frames = self.list_images(path)
+        self.__frame_index = 0
+        self.__image = self.__frames[self.__frame_index]
+        self.__rect = self.__image.get_rect(topleft = position)
+        
+    @property
+    def image(self):
+        return self.__image
+
+    @property
+    def frame_index(self):
+        return self.__frame_index
+    
+    @property
+    def frames(self):
+        return self.__frames
+    
+    @property
+    def rect(self):
+        return self.__rect
+
+    @frame_index.setter
+    def frame_index(self, frame_index):
+        self.__frame_index = frame_index
+
+    @rect.setter
+    def rect(self, rect):
+        self.__rect = rect
+
+    @image.setter
+    def image(self, image):
+        self.__image = image
+
+    def list_images(self, path):
+        image_list = []
+        for _,_,image_files in walk(path):
+            for image in image_files:
+                full_path = path + '/' + image
+                image_surf = pygame.image.load(full_path).convert_alpha()
+                image_list.append(image_surf)
+
+        return image_list
+
+    def animate(self):
+        self.__frame_index += 0.15
+        if self.__frame_index >= len(self.__frames):
+            self.__frame_index = 0
+        self.__image = self.__frames[int(self.__frame_index)]
+
+    def update(self,level_shift):
+        self.animate()
+        self.rect.x += level_shift
+    
+
---
+++ versao_final/static_objects.py
@@ -0,0 +1,22 @@
+import pygame
+
+class StaticObject(pygame.sprite.Sprite):
+    def __init__(self, position, image):
+        super().__init__()
+        self.__image = image
+        self.__rect = self.image.get_rect(topleft = position)
+
+    @property
+    def image(self):
+        return self.__image
+
+    @image.setter
+    def image(self, image):
+        self.__image = image
+
+    @property
+    def rect(self):
+        return self.__rect
+
+    def update(self, x_shift):
+        self.rect.x += x_shift
